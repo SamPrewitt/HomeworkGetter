0e9753647e4ed7417e5029f6c53e0b64a346ee79
Sam Prewitt <prewittsr@appstate.edu>
2020-11-05 01:43:56 -0500

    Create GoogleCalendarPage.cs

---
+++ GoogleCalendarPage.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program1
+{
+    class GoogleCalendarPage
+    {
+        //holds all XPath, etc. for https://calendar.google.com
+    }
+}
